a42c7aa491152d3558967967badab63e0b4b0a30
EugeneShatalin <shatalin_ei@mail.ru>
2021-09-16 18:56:09 +0600

    feat: add solution mine-sweeper

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,35 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  const numericField = [];
+  for(let i = 0; i < matrix.length; i++) {
+    let count = 0;
+    numericField[i] = [];
+    for(let j = 0; j <  matrix[0].length; j++) {
+      if(matrix[i][j]) {
+        numericField[i][j] = 1;
+        continue;
+      }
+
+      if(!matrix[i][j]) {
+        for(let k = (i - 1 < 0 ? 0 : i - 1); k < (i + 2 >= matrix.length ? matrix.length : i + 2); k++) {
+          for(let n = (j - 1 < 0 ? 0 : j - 1); n < (j + 2 >= matrix[0].length ? matrix[0].length : j + 2); n++) {
+
+            if(matrix[k][n]) {
+              count += 1;
+            }
+          }
+        }
+
+        if(count) {
+          numericField[i][j] = count;
+          count = 0;
+        } else {
+          numericField[i][j] = 0;
+        }
+      }
+    }
+  }
+  return numericField;
 }
